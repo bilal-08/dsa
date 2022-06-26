47931c81129ded79e5a4de25259b4e80c4f0f67b
RedBeeツ <77657831+bilal-08@users.noreply.github.com>
2022-06-26 08:27:50 -0700

    fix(): remove unnecessary function

--- algorithms/selectionSort.c
+++ algorithms/selectionSort.c
@@ -1,17 +1,15 @@
 #include <stdio.h>
-#include "swap.c"
+
 int main(){
     int arr[8] = {35,45,29,28,27,15,26,32};
     int i,j;
     // same as bubble
-swap(&arr[2],&arr[3]);
-printf("%d, %d\n",arr[2],arr[3]);
     for(i=0;i<8;i++){
 
         // remember to use i+1 not j=1;
         for(j=i+1;j<8;j++){
              if(arr[j]>arr[i]){
-            printf("%d & %d\n",i,j);
+          //  printf("%d & %d\n",i,j);
             int tempo = arr[i];
             arr[i] = arr[j];
             arr[j] = tempo;
